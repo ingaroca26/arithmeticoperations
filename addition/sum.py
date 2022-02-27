c18b8364454e3c96a9bc6bd97ecbc47e22d74936
ingaroca26 <ingaroca26@outlook.com>
2022-02-26 22:45:44 -0500

    v0.0.7

--- addition/sum.py
+++ addition/sum.py
@@ -1,15 +1,5 @@
-def return_sum() -> int:
-    first_summand: int = 16
-    second_summand: int = 2
-    sum: int = first_summand + second_summand
-    return sum
-
-
-def print_sum() -> None:
-    sum: int = return_sum()
-    f_sum: str = f'The sum is: {sum}.'
-    print(f_sum)
-
-
-if __name__ == '__main__':
-    print_sum()
+first_summand: int = 16
+second_summand: int = 2
+sum: int = first_summand + second_summand
+f_sum: str = f'The sum is: {sum}.'
+print(f_sum)
