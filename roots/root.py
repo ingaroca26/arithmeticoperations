fa8f75535756477c98dcef604e86bc18b8087f1c
ingaroca26 <ingaroca26@outlook.com>
2022-02-27 00:31:14 -0500

    v0.1.2

--- roots/root.py
+++ roots/root.py
@@ -1,15 +1,5 @@
-def return_root() -> float:
-    radicand: int = 16
-    index: int = 2
-    root: float = pow(radicand, 1 / index)
-    return root
-
-
-def print_root() -> None:
-    root: float = return_root()
-    f_root: str = f'The root is: {root}.'
-    print(f_root)
-
-
-if __name__ == '__main__':
-    print_root()
+radicand: int = 16
+index: int = 2
+root: float = pow(radicand, 1 / index)
+f_root: str = f'The root is: {root}.'
+print(f_root)
