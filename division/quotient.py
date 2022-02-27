541b6a5b6aa258c0252501f6d81037dd26677b91
ingaroca26 <ingaroca26@outlook.com>
2022-02-26 23:40:02 -0500

    v0.1.0

--- division/quotient.py
+++ division/quotient.py
@@ -1,15 +1,5 @@
-def return_quotient() -> float:
-    dividend: int = 16
-    divisor: int = 2
-    quotient: float = dividend / divisor
-    return quotient
-
-
-def print_quotient() -> None:
-    quotient: float = return_quotient()
-    f_quotient: str = f'The quotient is: {quotient}.'
-    print(f_quotient)
-
-
-if __name__ == '__main__':
-    print_quotient()
+dividend: int = 16
+divisor: int = 2
+quotient: float = dividend / divisor
+f_quotient: str = f'The quotient is: {quotient}.'
+print(f_quotient)
