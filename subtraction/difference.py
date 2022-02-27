937ef2b1e6e3c96363def203d13bffbbacc03b4e
ingaroca26 <ingaroca26@outlook.com>
2022-02-26 23:04:14 -0500

    v0.0.8

--- subtraction/difference.py
+++ subtraction/difference.py
@@ -1,15 +1,5 @@
-def return_difference() -> int:
-    minuend: int = 16
-    subtrahend: int = 2
-    difference: int = minuend - subtrahend
-    return difference
-
-
-def print_difference() -> None:
-    difference: int = return_difference()
-    f_difference: str = f'The difference is: {difference}.'
-    print(f_difference)
-
-
-if __name__ == '__main__':
-    print_difference()
+minuend: int = 16
+subtrahend: int = 2
+difference: int = minuend - subtrahend
+f_difference: str = f'The difference is: {difference}.'
+print(f_difference)
