148a854065f49524b1f32a8956d8e99fa8dd7404
ingaroca26 <ingaroca26@outlook.com>
2022-02-27 00:09:47 -0500

    v0.1.1

--- exponentiation/power.py
+++ exponentiation/power.py
@@ -1,15 +1,5 @@
-def return_power() -> int:
-    base: int = 16
-    exponent: int = 2
-    power: int = pow(base, exponent)
-    return power
-
-
-def print_power() -> None:
-    power: int = return_power()
-    f_power: str = f'The power is: {power}.'
-    print(f_power)
-
-
-if __name__ == '__main__':
-    print_power()
+base: int = 16
+exponent: int = 2
+power: int = pow(base, exponent)
+f_power: str = f'The power is: {power}.'
+print(f_power)
