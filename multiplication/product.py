ac4c820ac93125ff58b7794e8540751531c52fb5
ingaroca26 <ingaroca26@outlook.com>
2022-02-26 23:21:23 -0500

    v0.0.9

--- multiplication/product.py
+++ multiplication/product.py
@@ -1,15 +1,5 @@
-def return_product() -> int:
-    multiplicand: int = 16
-    multiplier: int = 2
-    product: int = multiplicand * multiplier
-    return product
-
-
-def print_product() -> None:
-    product: int = return_product()
-    f_product: str = f'The product is: {product}.'
-    print(f_product)
-
-
-if __name__ == '__main__':
-    print_product()
+multiplicand: int = 16
+multiplier: int = 2
+product: int = multiplicand * multiplier
+f_product: str = f'The product is: {product}.'
+print(f_product)
